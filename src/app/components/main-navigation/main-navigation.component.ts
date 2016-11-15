62071a5d51a3aa1b140818cf63b8522de57d7ae3
sarvarbek <me@sarvar.guru>
2016-11-14 20:32:51 -0500

    Added main navigation component

---
+++ src/app/components/main-navigation/main-navigation.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-navigation',
+  templateUrl: './main-navigation.component.html',
+  styleUrls: ['./main-navigation.component.scss']
+})
+export class MainNavigationComponent {
+	
+	navigationLinks : Dictionary;
+
+	constructor() {
+		this.navigationLinks = {
+			'/': 'Home',
+		  	'/auto-refinance': 'Refinance Calculator',
+		  	'/tax-calculator': 'Tax Calculator',
+		  	'/savings-calculator': 'Savings Calculator'
+		}
+	  	
+  	}
+
+  	links() : Array<string> {
+		return Object.keys(this.navigationLinks);
+	}
+}
+
+interface Dictionary {
+    [ index: string ]: string
+}
