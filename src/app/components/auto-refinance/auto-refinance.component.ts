86988b4a373b29bbe3b7c5c4924a5b3e91b27d78
sarvarbek <me@sarvar.guru>
2016-11-15 21:56:03 -0500

    Added functions to calculate monthly payment, total interest and total cost of the auto loan

--- src/app/components/auto-refinance/auto-refinance.component.ts
+++ src/app/components/auto-refinance/auto-refinance.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+export class autoLoan {
+	balance: number;
+	apr: number;
+	term: number;
+}
 
 @Component({
   selector: 'app-auto-refinance',
   templateUrl: './auto-refinance.component.html',
   styleUrls: ['./auto-refinance.component.scss']
 })
-export class AutoRefinanceComponent implements OnInit {
 
-  constructor() { }
+export class AutoRefinanceComponent {
 
-  ngOnInit() {
-  }
+	currentAutoLoan: autoLoan = {
+		balance: 25000,
+		apr: 3,
+		term: 60
+	};
 
+	//let comparisonLoans: autoLoan[] = [];
+
+	private monthlyPayment( balance: number, apr: number, term: number ) : number {
+		let interest = (apr > 0) ? Number(apr) / 12 / 100 : 0;
+
+		return (apr > 0 && term > 0) ? balance * (interest * Math.pow(1 + interest, term)) / (Math.pow(1 + interest, term) - 1) : (apr > 0) ? balance + balance * (interest / 100) : (term > 0) ? balance / term : balance;
+	};
+
+	private totalInterest( balance: number, apr: number, term: number ) : number {
+		let monthlyRate = this.monthlyPayment(balance, apr, term);
+
+		return (term > 0) ? monthlyRate * term - balance : (apr > 0) ? balance * (apr / 100) : 0;
+	};
+
+	private totalCost( balance: number, apr: number, term: number ) : number {
+		let	interest = this.totalInterest(balance, apr, term);
+
+		return balance + interest;
+	};
 }
+
