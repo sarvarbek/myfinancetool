dfd5823c5c15fe13bd7e6f44cb5351531b82f25a
sarvarbek <me@sarvar.guru>
2016-11-17 22:12:47 -0500

    Added the ability to add loans to compare

--- src/app/components/auto-refinance/auto-refinance.component.ts
+++ src/app/components/auto-refinance/auto-refinance.component.ts
@@ -1,26 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 export class autoLoan {
+	id: number;
+	label: string;
 	balance: number;
 	apr: number;
 	term: number;
+
+	constructor() {
+		this.id = Number(Math.random().toString().substr(2, 9)),
+        this.label = 'Untitled',
+		this.balance = 25000,
+		this.apr = 3,
+		this.term = 60
+    }
 }
 
 @Component({
-  selector: 'app-auto-refinance',
-  templateUrl: './auto-refinance.component.html',
-  styleUrls: ['./auto-refinance.component.scss']
+	selector: 'app-auto-refinance',
+	templateUrl: './auto-refinance.component.html',
+	styleUrls: ['./auto-refinance.component.scss']
 })
 
 export class AutoRefinanceComponent {
 
-	currentAutoLoan: autoLoan = {
-		balance: 25000,
-		apr: 3,
-		term: 60
+	private currentAutoLoan: autoLoan = new autoLoan();
+	private comparisonLoans: autoLoan[] = [];
+	
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+	private addComparisonLoan() {
+		// Set the balance the same as current loan
+		let newAutoLoan = new autoLoan();
+		newAutoLoan.balance = this.currentAutoLoan.balance;
+
+		this.comparisonLoans.push(newAutoLoan);
 	};
 
-	//let comparisonLoans: autoLoan[] = [];
+	private removeComparisonLoan(id) {
+		let removedLoanIndex = this.comparisonLoans.map(function (x) { return x.id; }).indexOf(id);
+
+		if (removedLoanIndex > -1) {
+			this.comparisonLoans.splice(removedLoanIndex, 1);
+		}
+	};
 
 	private monthlyPayment( balance: number, apr: number, term: number ) : number {
 		let interest = (apr > 0) ? Number(apr) / 12 / 100 : 0;
