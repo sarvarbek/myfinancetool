6662ec1abf3183c2ee2d6ccb2f269aaed278fd59
tashbaqa <sarvarni@gmail.com>
2016-11-17 22:18:25 -0500

    Refactored function parameters

--- src/app/components/auto-refinance/auto-refinance.component.ts
+++ src/app/components/auto-refinance/auto-refinance.component.ts
@@ -48,20 +48,27 @@ export class AutoRefinanceComponent {
 		}
 	};
 
-	private monthlyPayment( balance: number, apr: number, term: number ) : number {
-		let interest = (apr > 0) ? Number(apr) / 12 / 100 : 0;
+	private calcMonthlyPayment( loan ) : number {
+		let balance = loan.balance;
+		let apr = loan.apr;
+		let term = loan.term;
+		let monthlyInterest = (apr > 0) ? Number(apr) / 12 / 100 : 0;
 
-		return (apr > 0 && term > 0) ? balance * (interest * Math.pow(1 + interest, term)) / (Math.pow(1 + interest, term) - 1) : (apr > 0) ? balance + balance * (interest / 100) : (term > 0) ? balance / term : balance;
+		return (apr > 0 && term > 0) ? balance * (monthlyInterest * Math.pow(1 + monthlyInterest, term)) / (Math.pow(1 + monthlyInterest, term) - 1) : (apr > 0) ? balance + balance * (monthlyInterest / 100) : (term > 0) ? balance / term : balance;
 	};
 
-	private totalInterest( balance: number, apr: number, term: number ) : number {
-		let monthlyRate = this.monthlyPayment(balance, apr, term);
+	private calcTotalInterest( loan ) : number {
+		let balance = loan.balance;
+		let apr = loan.apr;
+		let term = loan.term;
+		let monthlyRate = this.calcMonthlyPayment(loan);
 
 		return (term > 0) ? monthlyRate * term - balance : (apr > 0) ? balance * (apr / 100) : 0;
 	};
 
-	private totalCost( balance: number, apr: number, term: number ) : number {
-		let	interest = this.totalInterest(balance, apr, term);
+	private calcTotalCost( loan ) : number {
+		let balance = loan.balance;
+		let	interest = this.calcTotalInterest(loan);
 
 		return balance + interest;
 	};
