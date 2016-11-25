64ab9e641ce571fcb8437fa1546eeddc973c1ef9
sarvarbek <me@sarvar.guru>
2016-11-24 21:28:46 -0500

    Added tax-savings route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { AutoRefinanceComponent } from './components/auto-refinance/auto-refinance.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { TaxSavingsComponent } from './components/tax-savings/tax-savings.component';
 
 export const AppRoutes = [
 	{ path: '', component: HomePageComponent },
-	{ path: 'auto-refinance', component: AutoRefinanceComponent }
+	{ path: 'auto-refinance', component: AutoRefinanceComponent },
+	{ path: 'tax-saving', component: TaxSavingsComponent }
 ]; 
