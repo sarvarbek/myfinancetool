5b96710907eb0d1ddf2ff00fa8cc5058a2675b9d
tashbaqa <sarvarni@gmail.com>
2016-11-24 21:23:05 -0500

    Added a percent difference directive

---
+++ src/app/directives/percent-difference.directive.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { PercentDifferenceDirective } from './percent-difference.directive';
+
+describe('Directive: PercentDifference', () => {
+  it('should create an instance', () => {
+    let directive = new PercentDifferenceDirective();
+    expect(directive).toBeTruthy();
+  });
+});
